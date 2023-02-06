08c39660d52d30017e7856c944515a98d1e4e182
Win Feigle <win.feigle@gmail.com>
2023-02-06 10:58:07 -0700

    commit tree 66c13a662d95c4eda8d8c247e2ac49589a677caa

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState} from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+
+function BandInput({ onBandSubmit } ) {
+  const [ formData, setFormData ] = useState("")
+
+  const onFormChange = (e) => {
+    setFormData(e.target.value)
+  }
+
+  return(
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          onBandSubmit(formData)
+          setFormData("")
+        }}
+        >
+        <p>
+          <label>name
+            <input 
+              type="text"
+              value={formData.name}
+              name="name"
+              onChange={onFormChange}
+              />
+          </label>
+        </p>
+        <button type="submit">add band</button>
+      </form>
+    </div>
+  );
 }
 
 export default BandInput;
--- src/features/bands/bandsSlice.js
+++ src/features/bands/bandsSlice.js
@@ -1,15 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const bandsSlice = createSlice({
+
+
   name: "bands",
   initialState: {
     entities: [],
   },
   reducers: {
-    // create reducer methods
+    bandAdded(state, action) {
+      state.entities.push(action.payload)
+    },
   },
 });
 
 // export the action creators
+export const { bandAdded } = bandsSlice.actions;
 
 export default bandsSlice.reducer;
