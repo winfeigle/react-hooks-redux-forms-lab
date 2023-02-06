f28ade57b0a65e264c9bd7bad77469f9d0270830
Win Feigle <win.feigle@gmail.com>
2023-02-06 10:58:14 -0700

    Complete lab

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,29 @@
 import React from "react";
+import BandInput from "./BandInput";
+import { useDispatch, useSelector } from "react-redux";
+import { bandAdded } from "./bandsSlice";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+    const dispatch = useDispatch();
+    const bands = useSelector((state) => state.bands.entities)
+
+    const onBandSubmit = (formData) => {
+      dispatch(bandAdded(formData))
+    }
+
+
+  return(
+    <div>
+      <BandInput 
+        onBandSubmit={onBandSubmit}
+        />
+      <ul>
+        {
+          bands.map((band) => <li key={band}>{band}</li>)
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default BandsContainer;
